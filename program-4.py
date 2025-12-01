9aeca5f3a80b0fc4262cc565131d9009561323bc
Arathy <arathyrajeesh2@gmail.com>
2025-12-01 19:25:54 +0530

    program4 added

--- program-4.py
+++ program-4.py
@@ -4,8 +4,8 @@ output = {}
 
 for i in range(1, 10):
     count = 0
-    for num in numbers:
-        if num % i == 0:
+    for n in numbers:
+        if n % i == 0:
             count += 1
     output[i] = count
 
